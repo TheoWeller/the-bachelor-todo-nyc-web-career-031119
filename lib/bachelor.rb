6ac7754844de88cf0c4e5ef9c6fbfff32be9703d
Theo Weller <weller.theodor@gmail.com>
2019-03-05 15:15:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,8 +36,16 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
-end
+
+  data.each do |season_num, value_array|
+      value_array.each do |contestant|
+        if contestant["hometown"] == hometown
+        contestant["occupation"]
+          end
+        end
+      end
+    end
+
 
 def get_average_age_for_season(data, season)
   # code here
