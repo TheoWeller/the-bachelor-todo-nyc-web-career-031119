7597ecea6ebe094d212d5bd3e3fd41f9d28b0fd2
Theo Weller <weller.theodor@gmail.com>
2019-03-05 15:01:55 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,20 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+  data.each do |season_num, value_array|
+
+    if season_num == season
+      value_array.each do |contestant|
+        if contestant["status"] == "Winner"
+
+            return contestant["name"].split(" ")[0]
+
+
+          end
+        end
+      end
+    end
+  end
+
 
 def get_contestant_name(data, occupation)
   # code here
